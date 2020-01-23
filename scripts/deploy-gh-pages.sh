dfd406b5063f1a5e08e01c4c8bcf1ae359a3099a
misky1986 <mskydt86@gmail.com>
2020-01-23 22:44:23 +0100

    Corrected minor error

--- scripts/deploy-gh-pages.sh
+++ scripts/deploy-gh-pages.sh
@@ -3,6 +3,7 @@ git init
 git config --global user.email $(gh_email) > /dev/null 2>&1
 git config --global user.name $(gh_user) > /dev/null 2>&1
 git add .
+git checkout deploy-sh-pages.sh
 git commit -m "[Azure Pipeline] Deploy to ghpages branch from Deploy stage"
 git remote add origin git@github.com:$(gh_user)/$(gh_repo).git
 echo "[Azure Pipeline] Remote origin added"
