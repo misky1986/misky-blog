e41deebb4bc299837e90ca1ba4da69b9ca2be766
misky1986 <mskydt86@gmail.com>
2020-01-23 22:36:32 +0100

    Refactored the azurepipeline deploy script

--- scripts/deploy-gh-pages.sh
+++ scripts/deploy-gh-pages.sh
@@ -1,35 +1,9 @@
-#! /bin/bash
 set -e # stop at first failing command
-
-echo "Build new hugo site"
-hugo -v
-cd public
-
-echo "Show files in public folder"
-ls -al
-
-echo "What folder are we in?"
-pwd
-
 git init
-echo "Show files in public folder after git init command"
-ls -al
-
-git config --global user.email "$GH_EMAIL" > /dev/null 2>&1
-git config --global user.name "$GH_NAME" > /dev/null 2>&1
-
-echo "show git status"
-git status
-
+git config --global user.email $(gh_email) > /dev/null 2>&1
+git config --global user.name $(gh_user) > /dev/null 2>&1
 git add .
-
-echo "show git status after add ."
-git status
-
-git commit -m "Deploy from CircleCI"
-
-echo "Remote addedd ."
-git remote add origin git@github.com:misky1986/misky-blog.git
-
-# use --quiet to avoid printing token in build logs
+git commit -m "[Azure Pipeline] Deploy to ghpages branch from Deploy stage"
+git remote add origin git@github.com:$(gh_user)/$(gh_repo).git
+echo "[Azure Pipeline] Remote origin added"
 git push --force origin master:gh-pages
